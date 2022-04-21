9294d60d58f354e49b05f4e01efce7ec7be26caa
mezgebeabebe <mezgebedingil@outlook.com>
2022-04-21 13:12:42 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * infinite_add - add two numbers.
@@ -12,37 +13,44 @@
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int add = 0, len1, len2, i, j;
+int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-for (len1 = 0; n1[len1]; len1++);
-for (len2 = 0; n2[len2]; len2++);
-
-if (len1 > size_r || len2 > size_r)
-return (0);
-
-len1--;
-len2--;
-size_r--;
-
-for (i = 0; i < size_r; i++, len1--, len2--)
-{
-if (len1 >= 0)
-	add += n1[len1] - '0';
-if (len2 >= 0)
-	add += n2[len2] - '0';
-if (len1 < 0 && len2 < 0 && add == 0)
-	break;
-r[i] = add % 10 + '0';
-add /= 10;
-}
-r[i] = '\0';
-if (len1 >= 0 || len2 >= 0 || add)
+while (*(n1 + c1) != '\0')
+	c1++;
+while (*(n2 + c2) != '\0')
+	c2++;
+if (c1 >= c2)
+	bg = c1;
+else
+	bg = c2;
+if (size_r <= bg + 1)
 	return (0);
-	for (i--, j = 0; i > j; i--, j++)
+r[bg + 1] = '\0';
+c1--, c2--, size_r--;
+dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+while (bg >= 0)
 {
-add = r[i];
-r[i] = r[j];
-r[j] = add;
+op = dr1 + dr2 + add;
+if (op >= 10)
+	add = op / 10;
+else
+	add = 0;
+if (op > 0)
+	*(r + bg) = (op % 10) + 48;
+else
+	*(r + bg) = '0';
+if (c1 > 0)
+	c1--, dr1 = *(n1 + c1) - 48;
+else
+	dr1 = 0;
+if (c2 > 0)
+	c2--, dr2 = *(n2 + c2) - 48;
+else
+	dr2 = 0;
+bg--, size_r--;
 }
+if (*(r) == '0')
+return (r + 1);
+else
 return (r);
 }
