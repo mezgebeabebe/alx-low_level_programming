12920df1c3225659b610b4070d0d98d8408d0b99
mezgebeabebe <mezgebedingil@outlook.com>
2022-04-21 02:40:54 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,8 +1,3 @@
-/*
- * File: 101-print_number.c
- * Auth: Brennan D Baraban
- */
-
 include "main.h"
 
 /**
@@ -14,17 +9,15 @@ include "main.h"
 
 void print_number(int n)
 {
+unsigned int num = n;
 
-	unsigned int num = n;
-
-	if (n < 0)
-	{
-	_putchar('-');
-	num = -num;
-	}
-
-	if ((num / 10) > 0)
-		print_number(num / 10);
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+if ((num / 10) > 0)
+	print_number(num / 10);
 
 	_putchar((num % 10) + '0');
 }
