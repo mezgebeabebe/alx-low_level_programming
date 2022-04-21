1db35640c4a98e237cd7dc6a81f64c8bcd267055
mezgebeabebe <mezgebedingil@outlook.com>
2022-04-21 12:47:45 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,55 +1,48 @@
 #include "main.h"
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
 /**
- * add_strings - Adds the numbers stored in two strings.
- * @n1: The string containing the first number to be added.
- * @n2: The string containing the second number to be added.
- * @r: The buffer to store the result.
- * @r_index: The current index of the buffer.
+ * infinite_add - add two numbers.
  *
- * Return: If r can store the sum - a pointer to the result.
- * If r cannot store the sum - 0.
+ * @n1: first number.
+ * @n2: second number.
+ * @r: result.
+ * @size_r: result size.
+ * Return: the addition of n1 and n2.
  */
 
-char *add_strings(char *n1, char *n2, char *r, int r_index)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-int num, tens = 0;
+int add = 0, len1, len2, i, j;
 
-for (; *n1 && *n2; n1--, n2--, r_index--)
+for (len1 = 0; n1[len1]; len1++);
+for (len2 = 0; n2[len2]; len2++);
+
+if (len1 > size_r || len2 > size_r)
+return (0);
+
+len1--;
+len2--;
+size_r--;
+
+for (i = 0; i < size_r; i++, len1--, len2--)
 {
-num = (*n1 - '0') + (*n2 - '0');
-num += tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
+if (len1 >= 0)
+	add += n1[len1] - '0';
+if (len2 >= 0)
+	add += n2[len2] - '0';
+if (len1 < 0 && len2 < 0 && add == 0)
+	break;
+r[i] = add % 10 + '0';
+add /= 10;
 }
-
-for (; *n1; n1--, r_index--)
-{
-num = (*n1 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
-
-for (; *n2; n2--, r_index--)
-{
-num = (*n2 - '0') + tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
-
-if (tens && r_index >= 0)
-{
-*(r + r_index) = (tens % 10) + '0';
-return (r + r_index);
-}
-
-else if (tens && r_index < 0)
+r[i] = '\0';
+if (len1 >= 0 || len2 >= 0 || add)
 	return (0);
-
-	return (r + r_index + 1);
+	for (i--, j = 0; i > j; i--, j++)
+{
+add = r[i];
+r[i] = r[j];
+r[j] = add;
 }
-
-
+return (r);
+}
