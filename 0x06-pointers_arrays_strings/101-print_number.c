ab73db830c6d675031ae310214f74650f9c526a2
mezgebeabebe <mezgebedingil@outlook.com>
2022-04-21 04:48:24 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -10,16 +10,14 @@
 
 void print_number(int n)
 {
-int a;
+unsigned int num = n;
 
 if (n < 0)
 {
 _putchar('-');
-a = n * -1;
+num = -num;
 }
-else
-a = n;
-if (n / 10)
-	print_number(a / 10);
-	_putchar(a % 10 + '0');
+if ((num / 10) > 0)
+	print_number(num / 10);
+	_putchar((num % 10) + '0');
 }
